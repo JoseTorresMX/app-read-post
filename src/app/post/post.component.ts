41b95090b2f71abe07cc595e27d39429ca8251ff
JoseTorres <joseangel.tosa@outlook.com>
2024-11-12 21:16:23 -0700

    configuracion del componente, ok

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from '../post.service';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrl: './post.component.css'
+  styleUrl: './post.component.css',
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
+  posts: any[] = [];
+  constructor(private postService: PostService) {}
 
+  ngOnInit() {
+    this.postService.getPosts().subscribe((data:any) => {
+      this.posts = data;//Guarda la lista de publicaciones en la variable
+    });
+  }
 }
